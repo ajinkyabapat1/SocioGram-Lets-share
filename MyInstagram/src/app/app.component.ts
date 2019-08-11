53127c2aedc2f072509036d6c16b88877c63bb5a
Ajinkya Bapat <ajinkyabapat1@gmail.com>
2019-08-11 16:45:21 +0530

    Routeguard -1

--- MyInstagram/src/app/app.component.ts
+++ MyInstagram/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'Firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MyInstagram';
+  s
+  ngOnInit(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyCmzFsM_-jY-mf-gp26AluFVUl8uBqE3to",
+      authDomain: "sociogram-f7a57.firebaseapp.com",
+      databaseURL: "https://sociogram-f7a57.firebaseio.com",
+      projectId: "sociogram-f7a57",
+      storageBucket: "",
+      messagingSenderId: "385438823498",
+      appId: "1:385438823498:web:d4f9c9b8619217dd"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+ 
 }
--- MyInstagram/src/app/auth/routeGaurd.ts
+++ MyInstagram/src/app/auth/routeGaurd.ts
@@ -1,11 +1,16 @@
 
 import { CanActivate } from '@angular/router/src/utils/preactivation';
+import * as firebase from 'Firebase';
 
 export class Routeguard implements CanActivate{
     path: import("@angular/router").ActivatedRouteSnapshot[];    
     route: import("@angular/router").ActivatedRouteSnapshot;
 canActivate(){
-    return false;
+    if (firebase.auth().currentUser){
+        return  true
+    }
+    else
+    return false
 }
 
 }
